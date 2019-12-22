fb2646ba016a95a4a05b44a3da7e90e15309c07c
Harishwarrior <warriorharish95668@gmail.com>
2019-12-22 12:47:52 +0530

    Checking authentication

--- app/src/main/java/com/e_rationqueue/app/MainActivity.java
+++ app/src/main/java/com/e_rationqueue/app/MainActivity.java
@@ -2,13 +2,42 @@ package com.e_rationqueue.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(),Password.getText().toString());
+            }
+        });
+    }
+    private void validate(String Username , String Password ){
+        if ((Username.equals("admin")) && (Password.equals("pass"))){
+            Intent intent =new Intent (MainActivity.this,SecondActivity.class);
+            startActivity(intent);
+
+        }else{
+            Login.setEnabled(false);
+        }
     }
 }
